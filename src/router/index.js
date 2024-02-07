fe1f4b9b28cf7d9e74ba60b02e696074b20da8ef
atheo810 <atheos810@gmail.com>
2024-02-07 15:39:33 +0800

    feat: about children to dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
-import SettingView from "@/views/SettingView.vue";
 import LoginView from "@/views/LoginView.vue";
 import DashboardView from "@/views/DashboardView.vue";
 const router = createRouter({
@@ -20,22 +19,19 @@ const router = createRouter({
       },
     },
     {
-      path: "/setting",
-      name: "setting",
-      component: SettingView,
-    },
-    {
       path: "/dashboard",
       name: "dasboard",
       component: DashboardView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      children: [
+        {
+          path: "about",
+          name: "about",
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import("../views/AboutView.vue"),
+        },
+      ],
     },
   ],
 });
